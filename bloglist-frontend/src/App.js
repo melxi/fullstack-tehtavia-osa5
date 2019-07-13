9462cf24f5f2eccbfb3cf3ef57a9c9ae6870b0f2
Melxi <magomed.futijev@gmail.com>
2019-07-13 11:18:30 +0300

    5.8*: blogilistan frontend, step8

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -161,7 +161,7 @@ function App() {
             addBlog={addBlog}
           />
         </Togglable>
-        {blogs.map(blog => 
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog => 
           <Blog key={blog.id} blog={blog} likeBlog={likeBlog}/>
         )}
       </div>
